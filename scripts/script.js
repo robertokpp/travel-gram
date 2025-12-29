0780fff2c3359d1db5736bcc0671adb8117f6c2a
Roberto Kennedy <roberto.kpp@gmail.com>
2025-12-29 18:06:44 -0500

    refatora lightbox e script para melhor gerenciamento de imagens

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,15 @@
-const gallery = document.querySelectorAll("img")
-gallery.forEach(element => {
+const gallery = document.querySelector("#gallery")
+const lightbox = document.querySelector(".lightbox")
+const lightboxContent = document.querySelector(".lightbox-content")
+const galleryImg = gallery.querySelectorAll("img")
+
+
+galleryImg.forEach(element => {
   element.addEventListener("click", () =>{
-    const srcImg = element.getAttribute("src")
+    const srcImg = element
     console.log(srcImg)
-    
-
-
+    lightboxContent.append(srcImg)
+    lightbox.style.display = "flex"
   })
 });
+
